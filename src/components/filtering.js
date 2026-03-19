337b79a4c56def1d89de7e7901354ffbfe2a1737
Danila Kudriashov <kudriashovdanilaie@gmail.com>
2026-03-19 15:06:20 +0100

    main fixes

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,31 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        let opt = document.createElement("option");
+        opt.textContent = name;
+        opt.value = name;
+        return opt; // @todo: создать и вернуть тег опции
+      }),
+    );
+  });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const parentElement = action.parentElement;
+      const input = parentElement.querySelector("input");
+      input.value = "";
+      state[action.dataset.field] = "";
     }
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter((row) => compare(row, state));
+  };
 }
