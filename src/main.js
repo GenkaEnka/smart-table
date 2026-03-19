8210747dc91387b4389c4b7ecca639dfaaa495d7
Danila Kudriashov <kudriashovdanilaie@gmail.com>
2026-03-19 15:30:43 +0100

    refactor: migrate data and main modules to async API-based workflow

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import "./fonts/ys-display/fonts.css";
 import "./style.css";
 
 import { data as sourceData } from "./data/dataset_1.js";
-
 import { initData } from "./data.js";
 import { processFormData } from "./lib/utils.js";
 
@@ -12,40 +11,28 @@ import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
 import { initSearching } from "./components/searching.js";
 
-const { data, ...indexes } = initData(sourceData);
+const api = initData(sourceData);
 
-/**
- * Сбор и обработка полей из таблицы
- * @returns {Object}
- */
 function collectState() {
   const state = processFormData(new FormData(sampleTable.container));
-
   const rowsPerPage = parseInt(state.rowsPerPage);
   const page = parseInt(state.page ?? 1);
   const total = [parseFloat(state.totalFrom), parseFloat(state.totalTo)];
-
-  return {
-    ...state,
-    rowsPerPage,
-    page,
-    total,
-  };
+  return { ...state, rowsPerPage, page, total };
 }
 
-/**
- * Перерисовка состояния таблицы при любых изменениях
- * @param {HTMLButtonElement?} action
- */
-function render(action) {
+async function render(action) {
   let state = collectState();
-  let result = [...data];
-  result = applySearching(result, state, action);
-  result = applyFiltering(result, state, action);
-  result = applySorting(result, state, action);
-  result = applyPagination(result, state, action);
+  let query = {};
 
-  sampleTable.render(result);
+  query = applySearching(query, state, action);
+  query = applyFiltering(query, state, action);
+  query = applySorting(query, state, action);
+  query = applyPagination(query, state, action);
+
+  const { total, items } = await api.getRecords(query);
+  updatePagination(total, query);
+  sampleTable.render(items);
 }
 
 const sampleTable = initTable(
@@ -58,8 +45,7 @@ const sampleTable = initTable(
   render,
 );
 
-// инициализация
-const applyPagination = initPagination(
+const { applyPagination, updatePagination } = initPagination(
   sampleTable.pagination.elements,
   (el, page, isCurrent) => {
     const input = el.querySelector("input");
@@ -76,13 +62,18 @@ const applySorting = initSorting([
   sampleTable.header.elements.sortByTotal,
 ]);
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-  searchBySeller: indexes.sellers,
-});
-
+const { applyFiltering, updateIndexes } = initFiltering(sampleTable.filter.elements);
 const applySearching = initSearching("search");
 
 const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+  const indexes = await api.getIndexes();
+  updateIndexes(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+  });
+  render();
+}
+
+init();
